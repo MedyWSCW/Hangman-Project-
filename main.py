68ceaea343093eb6261e61d7385b0d117891b796
DaequanWSCW <sud@wsc.school.nz>
2024-06-17 11:33:48 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-
-def main():
-    welcome = ['Welcome to our hangman game!. A random theme will be chosen and ',
-             'a random word will be chosen that fits the theme. ']
+answer = input ("Hello, welcome to our Hangman game. Do you know how to play Hangman?").upper().lower()
+if answer == "yes":
+    print("Great, our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
+    input("press enter to start")
+if answer == "no":
+    print("The aim of this game is to guess a random word before the hangman has been drawn.")
+    print("Our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
+    input("Press enter to start")
