fa62149d0c21170b311154bdd81dc7ec6f2393c8
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-06-17 17:58:37 +1200

    chnagws

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# Code for selecting the word from theme
+import random 
+themes = {
+    "Superheros/Villians ": ["Spiderman", "Deadpool", "", "Riddler", "Thanos"],
+    "Animals": ["Elephant", "Giraffe", "", "Lion", "Gorilla"],
+    "Dinosaurs": ["Velociraptor", "Triceratops", "Spinosaurus", "", "Tyrannosaurus"]
+}
+def choose_word():
+    theme = random.choice(list(themes.keys()))
+    word = random.choice(themes[theme]).upper()
+    return theme, word
+
 # Welcome the player to the quiz
 answer = input ("Hello, welcome to our Hangman game. Do you know how to play Hangman?").upper().lower()
 # Tell the player the themes on the game if they say yes
