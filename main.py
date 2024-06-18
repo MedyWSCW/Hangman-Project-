60d8621ac067c81349dc7c932da70283abfb7ace
DaequanWSCW <sud@wsc.school.nz>
2024-06-18 15:08:07 +1200

    asd

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Code for selecting the word from theme
 import random 
 themes = {
-    "Superheros/Villians ": ["Spiderman", "Deadpool", "", "Riddler", "Thanos"],
-    "Animals": ["Elephant", "Giraffe", "", "Lion", "Gorilla"],
+    "Superheros/Villians ": ["Spiderman", "Deadpool", "", "Riddler", "Thanos", "Batman"],
+    "Animals": ["Elephant", "Giraffe", "", "Lion", "Gorilla", "Tiger"],
     "Dinosaurs": ["Velociraptor", "Triceratops", "Spinosaurus", "", "Tyrannosaurus"]
 }
 def choose_word():
@@ -11,17 +11,45 @@ def choose_word():
     return theme, word
 
 theme, word = choose_word()
-print(f"Theme: {theme}, Word: {word}")
+
+Livesremaining = 11
+
+def main():
+    global Livesremaining
+
+
 
 # Welcome the player to the quiz
+
 answer = input ("Hello, welcome to our Hangman game. Do you know how to play Hangman?").upper().lower()
+
 # Tell the player the themes on the game if they say yes
+
 if answer == "yes":
     print("Great, our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
     input("press enter to start")
+else:
+    print("That isn't an answer")
+
 # Tell the player how to play the game if they answer no
+
 if answer == "no":
     print("The aim of this game is to guess a random word before the hangman has been drawn.")
     print("Our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
     input("Press enter to start")
+
+  
+print ("The chosen theme is {}".format (theme))
+
+
+print("The game will now start.")
+
+
+
+
+
+if Livesremaining == 0:
+
+    print("Game over")
+
 
