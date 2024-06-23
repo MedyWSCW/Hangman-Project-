9b4fd3c69301b40df003d9c6f4ab98292a2e922a
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-06-24 11:54:49 +1200

    change

--- main.py
+++ main.py
@@ -17,39 +17,33 @@ Livesremaining = 11
 def main():
     global Livesremaining
 
+    # Welcome the player to the quiz
+
+    answer = input ("Hello, welcome to our Hangman game. Do you know how to play Hangman?").upper().lower()
+
+    # Tell the player the themes on the game if they say yes
+
+    if answer == "yes":
+        print("Great, our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
+        input("press enter to start")
+    else:
+        print("That isn't an answer")
+
+    # Tell the player how to play the game if they answer no
+
+    if answer == "no":
+        print("The aim of this game is to guess a random word before the hangman has been drawn.")
+        print("Our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
+        input("Press enter to start")
+
+    
+    print ("The chosen theme is {}".format (theme))
 
 
-# Welcome the player to the quiz
+    print("The game will now start.")
 
-answer = input ("Hello, welcome to our Hangman game. Do you know how to play Hangman?").upper().lower()
+    if Livesremaining == 0:
 
-# Tell the player the themes on the game if they say yes
-
-if answer == "yes":
-    print("Great, our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
-    input("press enter to start")
-else:
-    print("That isn't an answer")
-
-# Tell the player how to play the game if they answer no
-
-if answer == "no":
-    print("The aim of this game is to guess a random word before the hangman has been drawn.")
-    print("Our Hangman game will have different themes which are animals, villains, superheroes, dinosaurs and cartoons.")
-    input("Press enter to start")
-
-  
-print ("The chosen theme is {}".format (theme))
-
-
-print("The game will now start.")
-
-
-
-
-
-if Livesremaining == 0:
-
-    print("Game over")
+        print("Game over")
 
 
